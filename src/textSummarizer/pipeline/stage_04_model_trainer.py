00fbbb4499673d082fd5f038837010223bb4526d
el-gabry <engineergabry@gmail.com>
2024-10-07 07:40:22 -0400

    model evaluation added

--- src/textSummarizer/pipeline/stage_04_model_trainer.py
+++ src/textSummarizer/pipeline/stage_04_model_trainer.py
@@ -1,14 +1,19 @@
 from textSummarizer.config.configuration import ConfigurationManager
 from textSummarizer.conponents.model_trainer import ModelTrainer
 from textSummarizer.logging import logger
-
+import os
 
 class ModelTrainerTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self):
-        config = ConfigurationManager()
-        model_trainer_config = config.get_model_trainer_config()
-        model_trainer_config = ModelTrainer(config=model_trainer_config)
-        model_trainer_config.train()
+        if  os.path.exists("artifacts/model_trainer"):
+            logger.info(f"model is alraedy trained>>>>>>>. Skipping training.")
+        else:
+            config = ConfigurationManager()
+            model_trainer_config = config.get_model_trainer_config()
+            model_trainer_config = ModelTrainer(config=model_trainer_config) 
+            model_trainer_config.train()
+
+            
